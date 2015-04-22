9dacd205202e64270fc70c0d93f852f79d3332b9
Lorena Pantano <lorena.pantano@gmail.com>
2015-04-22 17:10:49 -0400

    remove raw data

--- mirna/genome_based_mirna_annotation/star/map_star.sh
+++
@@ -1,3 +0,0 @@
-STAR --genomeDir /groups/bcbio/bcbio/genomes/Hsapiens/hg19/star --readFilesIn $1 --outFilterMultimapNmax 50 --outSAMattributes NH HI NM --alignIntronMax 1
-samtools view -Sbh Aligned.out.sam >| sim.20.hsa.bam
-bedtools intersect -bed -wo -s -f 0.80 -abam sim.20.hsa.bam -b hsa.gff3 >| sim.20.hsa.bam.anno
