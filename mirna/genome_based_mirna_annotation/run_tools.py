ab0a33b30787379f310e44258c12b76d2cfc0164
Lorena Pantano <lorena.pantano@gmail.com>
2015-05-15 18:35:42 -0400

    fix nonmapped/nonannotated reads

--- mirna/genome_based_mirna_annotation/run_tools.py
+++ mirna/genome_based_mirna_annotation/run_tools.py
@@ -19,12 +19,13 @@ def ch_directory(dir):
     os.chdir(cur_dir)
 
 
-def _stats(ann, fasta, prefix):
+def _stats(ann, bam, fasta, prefix):
     output = prefix + ".tsv"
     if not is_there(output):
         sim_data = res.read_sim_fa(fasta)
-        data, counts = res.read_bam(ann)
-        res.print_output(data, counts, sim_data, output, prefix)
+        data, counts = res.read_ann(ann)
+        mapped = res.read_bam(bam)
+        res.print_output(data, counts, sim_data, mapped, output, prefix)
     return output
 
 
@@ -52,7 +53,7 @@ def _star(input, index, mirbase):
             do.run(cmd_bam.format(**locals()), "")
 
         mirbase_output = _annotate(output, mirbase)
-    return mirbase_output
+    return (mirbase_output, output)
 
 
 def _bowtie2(input, index, mirbase):
@@ -69,7 +70,7 @@ def _bowtie2(input, index, mirbase):
             do.run(cmd_bam.format(**locals()), "")
 
         mirbase_output = _annotate(output, mirbase)
-    return mirbase_output
+    return (mirbase_output, output)
 
 
 def _hisat(input, index, mirbase):
@@ -86,7 +87,7 @@ def _hisat(input, index, mirbase):
             do.run(cmd_bam.format(**locals()), "")
 
         mirbase_output = _annotate(output, mirbase)
-    return mirbase_output
+    return (mirbase_output, output)
 
 
 if __name__ == "__main__":
@@ -113,5 +114,5 @@ if __name__ == "__main__":
     with open("summary.tsv", 'w') as out_handle:
         out_handle.write(res.H + "\n")
     for tool, stat in outputs.items():
-        stat_file = _stats(stat, args.fasta, tool)
+        stat_file = _stats(stat[0], stat[1], args.fasta, tool)
         do.run("cat %s >> summary.tsv" % stat_file, "merging %s" % tool)
